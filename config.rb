a322ff53b684f63b2d60c4e9bb38b932afeeab37
Norman <webmaster@grind-it.de>
2013-09-27 21:01:09 +0200

    Add compass config

---
+++ config.rb
@@ -0,0 +1,21 @@
+# HTTP paths
+http_path             = '/'
+http_stylesheets_path = '/css'
+http_images_path      = '/img'
+http_javascripts_path = '/js'
+
+# File system locations
+sass_dir              = 'src/scss'
+css_dir               = 'src/css'
+images_dir            = 'src/img'
+javascripts_dir       = 'src/js'
+
+# Set to true for easier debugging
+line_comments         = true
+
+# CSS output style - :nested, :expanded, :compact, or :compressed
+output_style          = :expanded
+
+# Determine whether Compass asset helper functions generate relative
+# or absolute paths
+relative_assets       = true
